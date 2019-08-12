7537fa865fa5afd24745f6b102e12942e22fb844
Andrew Olsen <olsen@olsen-macbookpro2.roam.corp.google.com>
2019-08-12 18:05:02 +0200

    Fix scaling on Java platform
    See Prospector for more details on scaling
    Added scripts so code can be downloaded and run easier.

--- spaceinvadersx/java/src/main/java/nz/ac/auckland/spaceinvadersx/java/SpaceInvadersXJava.java
+++ spaceinvadersx/java/src/main/java/nz/ac/auckland/spaceinvadersx/java/SpaceInvadersXJava.java
@@ -1,8 +1,14 @@
 package nz.ac.auckland.spaceinvadersx.java;
 
 import playn.java.LWJGLPlatform;
+import playn.core.Graphics;
+import playn.core.Canvas;
+import playn.core.Scale;
 
 import nz.ac.auckland.spaceinvadersx.core.SpaceInvadersX;
+import nz.ac.auckland.spaceinvadersx.graphics.Toolkit;
+
+import java.lang.reflect.Field;
 
 public class SpaceInvadersXJava {
 
@@ -12,7 +18,30 @@ public class SpaceInvadersXJava {
     config.width = SpaceInvadersX.WIDTH;
     config.height = SpaceInvadersX.HEIGHT;
     LWJGLPlatform plat = new LWJGLPlatform(config);
-    new SpaceInvadersX(plat);
+    JavaCanvasCreator cc = new JavaCanvasCreator(plat.graphics());
+    new SpaceInvadersX(plat, cc, null);
     plat.start();
+  }
+
+  static class JavaCanvasCreator implements Toolkit.CanvasCreator {
+    private final Graphics graphics; 
+    
+    JavaCanvasCreator(Graphics graphics) {
+      this.graphics = graphics;
+    }
+
+    public Canvas createCanvas(int pixelWidth, int pixelHeight) {
+      try {
+        Field f = Graphics.class.getDeclaredField("scale");
+        f.setAccessible(true);
+        Scale temp = (Scale) f.get(graphics);
+        f.set(graphics, Scale.ONE);
+        Canvas canvas = graphics.createCanvas(pixelWidth, pixelHeight);
+        f.set(graphics, temp);
+        return canvas;
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
